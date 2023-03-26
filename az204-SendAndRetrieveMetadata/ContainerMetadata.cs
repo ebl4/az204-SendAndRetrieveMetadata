f599c913536c30761b34043f64ea0d7d4d3bff90
Edson <edbdelima@gmail.com>
2023-03-26 16:10:25 -0300

    Adicionar arquivos de projeto.

---
+++ az204-SendAndRetrieveMetadata/ContainerMetadata.cs
@@ -0,0 +1,49 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+
+namespace az204_SendAndRetrieveMetadata
+{
+    public class ContainerMetadata
+    {
+        public static async Task AddContainerMetadataAsync(BlobContainerClient container)
+        {
+            try
+            {
+                IDictionary<string, string> metadata = new Dictionary<string, string>();
+
+                // Add some metadata to the container
+                metadata.Add("docType", "textDocuments");
+                metadata.Add("category", "guidence");
+
+                // Set the container's metadata
+                await container.SetMetadataAsync(metadata);
+
+            } catch (RequestFailedException e)
+            {
+                Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+
+        public static async Task ReadContainerMetadataAsync(BlobContainerClient containerClient)
+        {
+            try
+            {
+                var properties = await containerClient.GetPropertiesAsync();
+
+                // Enumerate the container's metadata
+                foreach (var metadataItem in properties.Value.Metadata)
+                {
+                    Console.WriteLine($"\tKey: {metadataItem.Key}");
+                    Console.WriteLine($"\tValue: {metadataItem.Value}");
+                }
+            } catch (RequestFailedException e)
+            {
+                Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ az204-SendAndRetrieveMetadata/Program.cs
@@ -0,0 +1,13 @@
+﻿using az204_SendAndRetrieveMetadata;
+using Azure.Storage.Blobs;
+
+string conectionString = "CONNECTION_STRING";
+
+BlobServiceClient blobServiceClient = new BlobServiceClient(conectionString);
+
+string containerName = "wtblob" + Guid.NewGuid().ToString();
+
+BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+
+await ContainerMetadata.AddContainerMetadataAsync(containerClient);
+await ContainerMetadata.ReadContainerMetadataAsync(containerClient);
